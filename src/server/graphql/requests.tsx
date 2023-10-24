a6b0fce802adf2594b3ff9edbb8b85e265453c28
Nichita Gutu <gutunichita757@gmail.com>
2023-10-24 14:15:17 +0200

    merge two graphql calls into one

--- src/server/graphql/requests.tsx
+++ src/server/graphql/requests.tsx
@@ -1,20 +1,14 @@
 import { gql } from "@apollo/client/core";
 import { client } from "./apolloClient";
 
-// Define the GraphQL queries
-const GET_INCOMING_ATTESTATIONS = gql`
-  query GetIncomingAttestations($subject: String!, $schemaId: String!) {
-    attestations(where: { subject: $subject, schemaId: $schemaId }) {
+const GET_ALL_ATTESTATIONS = gql`
+  query GetAllAttestations($address: String!, $schemaId: String!) {
+    incoming: attestations(where: { subject: $address, schemaId: $schemaId }) {
       decodedData
       subject
       attester
     }
-  }
-`;
-
-const GET_OUTGOING_ATTESTATIONS = gql`
-  query GetOutgoingAttestations($attester: String!, $schemaId: String!) {
-    attestations(where: { attester: $attester, schemaId: $schemaId }) {
+    outgoing: attestations(where: { attester: $address, schemaId: $schemaId }) {
       decodedData
       subject
       attester
@@ -28,26 +22,24 @@ interface Attestation {
   attester: string;
 }
 
-// BFS function to explore attestations
 async function bfsTraversal(
   startAddress: string,
   schemaId: string,
-  type: "incoming" | "outgoing",
   maxDepth: number = 2
 ): Promise<Map<string, Attestation[]>> {
   const visited = new Map<string, Attestation[]>();
-  let queue: { address: string; depth: number }[] = [];
-  queue.push({ address: startAddress, depth: 0 });
+  let queue: { address: string; depth: number }[] = [
+    { address: startAddress, depth: 0 },
+  ];
 
   console.log("Starting BFS traversal");
-
   console.log(startAddress);
 
   while (queue.length > 0) {
     const currentNode = queue.shift();
 
     console.log(currentNode);
-    // Avoid revisiting nodes and restrict by maxDepth
+
     if (
       !currentNode ||
       currentNode.depth >= maxDepth ||
@@ -56,68 +48,51 @@ async function bfsTraversal(
       continue;
     }
 
-    let result;
-    if (type === "incoming") {
-      result = await client.query({
-        query: GET_INCOMING_ATTESTATIONS,
-        variables: { subject: currentNode.address, schemaId: schemaId },
-      });
-    } else {
-      result = await client.query({
-        query: GET_OUTGOING_ATTESTATIONS,
-        variables: { attester: currentNode.address, schemaId: schemaId },
-      });
-    }
+    const result = await client.query({
+      query: GET_ALL_ATTESTATIONS,
+      variables: { address: currentNode.address, schemaId: schemaId },
+    });
 
-    const attestations: Attestation[] = result.data.attestations;
+    const allAttestations: Attestation[] = [
+      ...result.data.incoming,
+      ...result.data.outgoing,
+    ];
 
-    // Store the attestation data in the visited map.
-    if (attestations.length > 0) {
+    if (allAttestations.length > 0) {
       if (visited.has(currentNode.address)) {
         const existingAttestations = visited.get(currentNode.address);
-        visited.set(currentNode.address, [
-          ...existingAttestations,
-          ...attestations,
-        ]);
+        const mergedAttestations = [
+          ...new Set([...existingAttestations, ...allAttestations]),
+        ];
+        visited.set(currentNode.address, mergedAttestations);
       } else {
-        visited.set(currentNode.address, attestations);
+        visited.set(currentNode.address, allAttestations);
       }
     }
 
-    for (const attestation of attestations) {
-      const nextAddress =
-        type === "incoming" ? attestation.attester : attestation.subject;
-      if (!visited.has(nextAddress)) {
-        queue.push({ address: nextAddress, depth: currentNode.depth + 1 });
+    for (const attestation of allAttestations) {
+      const nextAddresses = [attestation.attester, attestation.subject].filter(
+        (addr) => addr !== currentNode.address
+      );
+
+      for (const nextAddress of nextAddresses) {
+        if (!visited.has(nextAddress)) {
+          queue.push({ address: nextAddress, depth: currentNode.depth + 1 });
+        }
       }
     }
   }
 
   console.log("Finished BFS traversal");
-
   console.log(visited);
 
-  return visited; // return the visited map
+  return visited;
 }
 
-export async function exploreIncomingAttestations(
+export async function exploreAllAttestations(
   startAddress: string,
   schemaId: string,
   maxDepth: number = 2
 ): Promise<Map<string, Attestation[]>> {
-  const data = await bfsTraversal(startAddress, schemaId, "incoming", maxDepth);
-
-  return data;
+  return await bfsTraversal(startAddress, schemaId, maxDepth);
 }
-
-export async function exploreOutgoingAttestations(
-  startAddress: string,
-  schemaId: string,
-  maxDepth: number = 2
-): Promise<Map<string, Attestation[]>> {
-  return await bfsTraversal(startAddress, schemaId, "outgoing", maxDepth);
-}
-
-// exploreIncomingAttestations("0xAddress", "schemaId", 2);
-
-// exploreOutgoingAttestations("0xAddress", "schemaId", 2);
--- src/server/index.ts
+++ src/server/index.ts
@@ -6,72 +6,49 @@ import { z } from "zod";
 
 import { todos } from "@/db/schema";
 import { eq } from "drizzle-orm";
-import { exploreIncomingAttestations, exploreOutgoingAttestations } from "./graphql/requests";
+import { exploreAllAttestations } from "./graphql/requests";
 
 const sqlite = new Database("sqlite.db");
 const db = drizzle(sqlite);
 
+export const appRouter = router({
+  getTodos: publicProcedure.query(async () => {
+    return db.select().from(todos).all();
+  }),
+  addTodo: publicProcedure.input(z.string()).mutation(async (opts) => {
+    db.insert(todos)
+      .values({
+        content: opts.input,
+        done: 0,
+      })
+      .run();
 
-// try {
-//     migrate(
-//         db,
-//         {
-//             migrationsFolder: "drizzle"
-//         }
-//     )
+    return true;
+  }),
 
-// } catch (e) {
-//     console.log(e);
-// }
+  getAttestations: publicProcedure.query(async () => {
+    const data = await exploreAllAttestations(
+      "0xBB60ADaFB45ebbf4CE60799950a39f3dfb3AD2DC",
+      "0x7644469043E6CE9F4D288DCF021AA6F9022075E15F6746FDFED8C8EBEED558EE",
+      2
+    );
 
-
-export const appRouter = router(
-    {
-        getTodos: publicProcedure.query(
-            async () => {
-                return db.select().from(todos).all();
-            }
-        ),
-        addTodo: publicProcedure.input(
-            z.string()
-        ).mutation(
-            async (opts) => {
-                db.insert(todos).values(
-                    {
-                        content: opts.input,
-                        done: 0,
-                    }
-                ).run();
-
-                return true;
-            }
-        ),
-
-        getAttestations: publicProcedure.query(
-            async () => {
-                const data = await exploreIncomingAttestations("0xBB60ADaFB45ebbf4CE60799950a39f3dfb3AD2DC", "0x47a5c13d25325ce9923ab417bb362e151422d02d6649cf8a8e873a0e2dba4065", 2);
-
-                return JSON.stringify(Object.fromEntries(data));
-            }
-        )
-        ,
-        setDone: publicProcedure.input(
-            z.object(
-                {
-                    id: z.number(),
-                    done: z.number(),
-                }
-            )
-        ).mutation(
-            async (opts) => {
-                db.update(todos).set({
-                    done: opts.input.done,
-
-                }).where(eq(todos.id, opts.input.id)).run();
-                return true;
-            }
-        )
-    }
-)
-
-
+    return JSON.stringify(Object.fromEntries(data));
+  }),
+  setDone: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        done: z.number(),
+      })
+    )
+    .mutation(async (opts) => {
+      db.update(todos)
+        .set({
+          done: opts.input.done,
+        })
+        .where(eq(todos.id, opts.input.id))
+        .run();
+      return true;
+    }),
+});
