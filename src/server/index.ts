5d2c0e95e5be304e26ca52c944a11e064dd2544a
qpwedev <russfdk@gmail.com>
2023-11-05 14:34:45 +0300

    wowo

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,32 +1,11 @@
 import { publicProcedure, router } from "./trpc";
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
 import { z } from "zod";
 
-import { todos } from "@/db/schema";
-import { eq } from "drizzle-orm";
 import { exploreAllAttestations } from "./graphql/getAddressRespectAttestations";
 import { matchEthAddress } from "@/app/_utils/matchEthAddress";
 import { getAddressLinks } from "./graphql/getAddressLinks";
 
-const sqlite = new Database("sqlite.db");
-const db = drizzle(sqlite);
-
 export const appRouter = router({
-  getTodos: publicProcedure.query(async () => {
-    return db.select().from(todos).all();
-  }),
-  addTodo: publicProcedure.input(z.string()).mutation(async (opts) => {
-    db.insert(todos)
-      .values({
-        content: opts.input,
-        done: 0,
-      })
-      .run();
-
-    return true;
-  }),
-
   getAttestations: publicProcedure.input(z.string()).query(async (opts) => {
     if (!matchEthAddress(opts.input)) {
       return null;
@@ -41,9 +20,6 @@ export const appRouter = router({
     return data;
   }),
 
-  // getAddressLinks(address);
-
-
   getAddressLinks: publicProcedure.input(z.string()).query(async (opts) => {
     if (!matchEthAddress(opts.input)) {
       return null;
@@ -55,23 +31,6 @@ export const appRouter = router({
 
     return data;
   }),
-
-  setDone: publicProcedure
-    .input(
-      z.object({
-        id: z.number(),
-        done: z.number(),
-      }),
-    )
-    .mutation(async (opts) => {
-      db.update(todos)
-        .set({
-          done: opts.input.done,
-        })
-        .where(eq(todos.id, opts.input.id))
-        .run();
-      return true;
-    }),
 });
 
 export type AppRouter = typeof appRouter;
